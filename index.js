2efd3f06c68ea4a2d67e90f58471e815f821aa32
Pawel Dziok <pawel.dziok@schibsted.pl>
2016-03-30 17:47:25 +0200

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const logger = console;
+
+class QueueConsumer {
+    constructor(config) {
+        this.queue = config.queue;
+        this.logger = config.logger || logger.log;
+        this.errorLogger = config.errorLogger || logger.error;
+    }
+
+    consume(callback) {
+        this.logger(`Consuming messages from ${this.queue.queueUrl}`);
+        const processMessage = (message) => {
+            return Promise.resolve()
+                .then(() => {
+                    return callback(message.Body, message.Attributes)
+                        .then(() => message);
+                })
+                .then(() => this.queue.deleteMessage(message))
+                .catch(err => {
+                    this.errorLogger('Error processing message %s. %o', message.MessageId, err);
+                });
+        };
+        this.queue.getNextNonEmptyBatch()
+            .then(messages =>
+                Promise.all(messages.map(m => processMessage(m, callback))))
+            .then(() => this.consume(callback))
+            .catch(err => {
+                this.errorLogger('Error processing messages: %o', err);
+            });
+    }
+}
+
+module.exports = QueueConsumer;
