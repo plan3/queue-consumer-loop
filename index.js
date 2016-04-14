a91596eda69105740b44405147a35e953ed844e4
lukasz.guzik <lukasz.guzik@schibsted.pl>
2016-04-14 10:27:02 +0200

    Refactor. Call `callback` inside a promise to let `catch` do its job.

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ class QueueConsumer {
 
     consume(callback) {
         this.logger.info(`Consuming messages from ${this.queue.queueUrl}`);
+
         const processMessage = (message) => {
-            return Promise.resolve(callback(message.Body, message.Attributes))
+            return Promise.resolve()
+                .then(() => callback(message.Body, message.Attributes))
                 .then(() => this.queue.deleteMessage(message))
-                .catch(err => {
+                .catch((err) => {
                     this.logger.error(`Error processing message ${message.MessageId}`, err, err.stack);
                 });
         };
+
         this.queue.getNextNonEmptyBatch()
-            .then(messages =>
-                Promise.all(messages.map(m => processMessage(m, callback))))
-            .then(() => this.consume(callback))
+            .then((messages) => Promise.all(messages.map(m => processMessage(m, callback))))
             .catch(err => {
                 this.logger.error(`Error processing messages`, err, err.stack);
-            });
+            })
+            .finally(() => this.consume(callback));
     }
 }
 
